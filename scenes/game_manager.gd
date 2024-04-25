db4f9f5b5f8083a3212b858fbe90154e0d87e22e
Omar-Munib <123440005+Omar-Munib@users.noreply.github.com>
2024-04-25 23:15:31 +0400

    pause menu added
    Added a pause menu
    with resume and exit game

--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+class_name GameManager
+
 @onready var points_label = %PointsLabel
 @onready var collectables_group = null
 var collectibles_collected: int = 0
@@ -41,3 +43,16 @@ func finish_line_available() -> bool:
 func collect_finish_line():
 	finish_line_collected = true
 
+signal toggle_game_paused(is_paused : bool)
+
+var game_paused : bool = false:
+	get:
+		return game_paused
+	set(value):
+		game_paused = value
+		get_tree().paused = game_paused
+		emit_signal("toggle_game_paused", game_paused)
+
+func _input(event : InputEvent):
+	if(event.is_action_pressed("pause")):
+		game_paused = !game_paused
--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,13 +1,22 @@
 extends Node
 
+var game_manager
+
+func _ready():
+	game_manager = get_node("GameManager")
+
 func _on_button_pressed():
+	game_manager.game_paused = false
 	get_tree().change_scene_to_file("res://scenes/level1.tscn")
 
 func _on_button_2_pressed():
+	game_manager.game_paused = false
 	get_tree().change_scene_to_file("res://scenes/level2.tscn")
 
 func _on_button_3_pressed():
+	game_manager.game_paused = false
 	get_tree().change_scene_to_file("res://scenes/level3.tscn")
 
 func _on_button_4_pressed():
+	game_manager.game_paused = false
 	get_tree().change_scene_to_file("res://scenes/control_screen.tscn")
