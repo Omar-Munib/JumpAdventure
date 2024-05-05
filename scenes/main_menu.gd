c673a364dcd85203bb27605c8705b3a03fde7e76
Omar-Munib <123440005+Omar-Munib@users.noreply.github.com>
2024-05-05 19:52:47 +0400

    new level select page
    new level 5 in the making

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -5,24 +5,14 @@ var game_manager
 func _ready():
 	game_manager = get_node("GameManager")
 
-func _on_button_pressed():
+func _on_level_select_button_pressed():
 	game_manager.game_paused = false
-	get_tree().change_scene_to_file("res://scenes/level1.tscn")
-
-func _on_button_2_pressed():
-	game_manager.game_paused = false
-	get_tree().change_scene_to_file("res://scenes/level2.tscn")
-
-func _on_button_3_pressed():
-	game_manager.game_paused = false
-	get_tree().change_scene_to_file("res://scenes/level3.tscn")
-
-func _on_level_4_button_pressed():
-	game_manager.game_paused = false
-	get_tree().change_scene_to_file("res://scenes/level4.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
 
 func _on_button_4_pressed():
 	game_manager.game_paused = false
 	get_tree().change_scene_to_file("res://scenes/control_screen.tscn")
+
+
 
 
---
+++ scripts/level_select.gd
@@ -0,0 +1,29 @@
+extends Node
+
+var game_manager
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	game_manager = get_node("GameManager")
+
+func _on_level_1_button_pressed():
+	game_manager.game_paused = false
+	get_tree().change_scene_to_file("res://scenes/level1.tscn")
+
+func _on_level_2_button_2_pressed():
+	game_manager.game_paused = false
+	get_tree().change_scene_to_file("res://scenes/level2.tscn")
+
+func _on_level_3_button_3_pressed():
+	game_manager.game_paused = false
+	get_tree().change_scene_to_file("res://scenes/level3.tscn")
+
+func _on_level_4_button_4_pressed():
+	game_manager.game_paused = false
+	get_tree().change_scene_to_file("res://scenes/level4.tscn")
+
+func _on_level_5_button_5_pressed():
+	pass # Replace with function body.
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
