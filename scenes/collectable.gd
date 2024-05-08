6674633b8adc0c32038699f61e5ad9476f75f047
Omar-Munib <123440005+Omar-Munib@users.noreply.github.com>
2024-05-08 21:27:24 +0400

    audio for collectables

--- scenes/collectable.gd
+++ scenes/collectable.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
 @onready var game_manager = %GameManager
+@onready var animation_player = $AnimationPlayer
 
 func _on_body_entered(body):
 	if (body.name == "CharacterBody2D"):
-		queue_free()
+		animation_player.play("pickup")
 		game_manager.add_point()
