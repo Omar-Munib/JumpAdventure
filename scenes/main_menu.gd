0c99180cd382c2743a6c8219716253258fe0e07f
Omar-Munib <123440005+Omar-Munib@users.noreply.github.com>
2024-05-09 23:07:43 +0400

    settings menu made for volume

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -9,10 +9,11 @@ func _on_level_select_button_pressed():
 	game_manager.game_paused = false
 	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
 
+func _on_settings_button_pressed():
+	game_manager.game_paused = false
+	get_tree().change_scene_to_file("res://scenes/settings_menu.tscn")
+
 func _on_button_4_pressed():
 	game_manager.game_paused = false
 	get_tree().change_scene_to_file("res://scenes/control_screen.tscn")
-
-
-
 
