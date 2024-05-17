a7782ead9e82bb862e6394cec8dce431f58c056b
Omar-Munib <123440005+Omar-Munib@users.noreply.github.com>
2024-05-17 19:30:20 +0400

    settings menu
    cant get the pause menu to hide when pressing the settings button

--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -1,10 +1,13 @@
 extends Control
 
 @export var game_manager : GameManager
+var settings_container
 
 func _ready():
 	hide()
 	game_manager.connect("toggle_game_paused", _on_game_manager_toggle_game_paused)
+	settings_container = get_node("Panel/VBoxContainer/SettingsButton/PanelContainer")
+	settings_container.hide()
 
 func _process(delta):
 	pass
@@ -16,13 +19,16 @@ func _on_game_manager_toggle_game_paused(is_paused : bool):
 		hide()
 
 func _on_settings_button_pressed():
-	var settings_container = get_node("Panel/VBoxContainer/SettingsButton/PanelContainer")
-	print(self.get_path())
 	if settings_container:
 		settings_container.show()
 		print("container found")
 	else:
 		print("container not found")
+
+func _on_back_button_pressed():
+	if settings_container:
+		settings_container.hide()
+		show()
 
 func _on_resume_button_pressed():
 	game_manager.game_paused = false
@@ -33,6 +39,8 @@ func _on_exit_button_pressed():
 	#game_manager.game_paused = false
 #to exit the game and shut it off use code below
 	#get_tree().quit() 
+
+
 
 
 
